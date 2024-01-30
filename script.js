c2d81aa2c3e96543c6d50f79ae5bab9634b5b52e
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-30 13:31:42 +0530

    Get references to HTML elements

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get references to HTML elements
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+
